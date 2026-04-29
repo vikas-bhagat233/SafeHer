4c8f97323f4545457ae64ed32817af145afbaf5f
vikas-bhagat233 <bhagatvikas554@gmail.com>
2026-04-30 02:26:07 +0530

    123

--- frontend/app/utils/constants.js
+++ frontend/app/utils/constants.js
@@ -2,7 +2,10 @@ import { Platform } from 'react-native';
 
 const DEFAULT_ANDROID_EMULATOR_URL = 'http://10.0.2.2:3000/api';
 const DEFAULT_LAN_URL = 'http://192.168.0.106:3000/api';
+const DEFAULT_PRODUCTION_URL = 'https://safeher-backend-b8oo.onrender.com/api';
 
 export const BASE_URL =
 	process.env.EXPO_PUBLIC_API_URL ||
-	(Platform.OS === 'android' ? DEFAULT_ANDROID_EMULATOR_URL : DEFAULT_LAN_URL);
+	(__DEV__
+		? (Platform.OS === 'android' ? DEFAULT_ANDROID_EMULATOR_URL : DEFAULT_LAN_URL)
+		: DEFAULT_PRODUCTION_URL);
